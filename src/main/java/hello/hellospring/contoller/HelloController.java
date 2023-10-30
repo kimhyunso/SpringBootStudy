133b348eca82a95823e7b1b99cd0ac1b9972ef26
kimhyunso <gustn9515@gmail.com>
2023-10-30 22:34:49 +0900

    20231030 22:34

---
+++ src/main/java/hello/hellospring/contoller/HelloController.java
@@ -0,0 +1,57 @@
+package hello.hellospring.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "spring!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @ResponseBody
+    @GetMapping("hello-string")
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; //"hello spring"
+    }
+
+
+
+    // 객체가 오면 json방식으로 반환
+    // JsonConverter :: 객체, StringConverter :: 문자
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
